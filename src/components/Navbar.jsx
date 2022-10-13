ea3afcf7f46513222d409165c8e9c66f1a2d2308
Mateo-Rey <jrey3darts@gmail.com>
2022-10-13 16:21:57 -0400

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
     const {currentUser} = useContext(AuthContext)
   return (
     <div className='navbar'>
-        <span className='logo'>Swift Chat</span>
+        <span className='logo'>Quick Chat</span>
         <div className='user'>
             <img src={currentUser?.photoURL}/>
             <span>{currentUser?.displayName}</span>
