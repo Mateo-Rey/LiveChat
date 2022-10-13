cbc7215d827480f2f01528c148ef40af147356af
Mateo-Rey <jrey3darts@gmail.com>
2022-10-13 16:23:51 -0400

    final

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Swift Chat</span>
+        <span className="logo">Quick Chat</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
